990dd87197896fb7f074fb9e7b4f0dc20a246ae0
sloegr <jerryprince981@gmail.com>
2025-11-03 16:40:21 +0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+import { meet } from "@googleworkspace/meet-addons/meet.addons";
+
+const CLOUD_PROJECT_NUMBER = "613174040862";
+const MAIN_STAGE_URL = "MAIN_STAGE_URL";
+
+/**
+ * Prepares the add-on Side Panel Client, and adds an event to launch the
+ * activity in the main stage when the main button is clicked.
+ */
+export async function setUpAddon() {
+  const session = await meet.addon.createAddonSession({
+    cloudProjectNumber: CLOUD_PROJECT_NUMBER,
+  });
+  const sidePanelClient = await session.createSidePanelClient();
+  document
+    .getElementById("start-activity")
+    .addEventListener("click", async () => {
+      await sidePanelClient.startActivity({
+        mainStageUrl: MAIN_STAGE_URL,
+      });
+    });
+}
+
+export async function initializeMainStage() {
+  const session = await meet.addon.createAddonSession({
+    cloudProjectNumber: CLOUD_PROJECT_NUMBER,
+  });
+  await session.createMainStageClient();
+}
