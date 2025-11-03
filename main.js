035bf300b7d0e5c339bc02777b14af61247f346b
sloegr <jerryprince981@gmail.com>
2025-11-03 17:52:31 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import { meet } from "@googleworkspace/meet-addons/meet.addons";
 
-const CLOUD_PROJECT_NUMBER = "613174040862";
+//const CLOUD_PROJECT_NUMBER = "613174040862";
+
+const CLOUD_PROJECT_NUMBER = "893187114358";
+
 const MAIN_STAGE_URL =
   "https://sloegr.github.io/hello-world-meet/MainPage.html";
 
