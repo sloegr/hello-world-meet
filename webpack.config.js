612f8feddb63d6c54a3f19cbcede16be00211c7f
sloegr <jerryprince981@gmail.com>
2025-11-04 14:27:14 +0400

    Bundled the files to webpack dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = {
   entry: "./main.js",
   output: {
     library: "helloWorld",
   },
   mode: "development",
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "SidePanel.html", to: "SidePanel.html" },
+        { from: "MainStage.html", to: "MainStage.html" },
+      ],
+    }),
+  ],
 };
