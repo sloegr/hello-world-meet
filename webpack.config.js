b4bb55c033e2caadd0e1e731c32d5eee72b8915a
sloegr <jerryprince981@gmail.com>
2025-11-05 12:06:44 +0400

    updated the files to align with official docs

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path");
 
 module.exports = {
   entry: "./main.js",
   output: {
     library: "helloWorld",
+    path: path.resolve(__dirname, "./dist/hello-world"),
   },
   mode: "development",
   plugins: [
